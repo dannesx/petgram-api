029c08561a944191566d9a3ddcffbac5abd550a1
dannesx <daniel.viantunes@gmail.com>
2024-12-14 00:14:31 -0300

    add cors configuration

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,6 +3,6 @@ import { login } from '../controllers/auth'
 
 const router = Router()
 
-router.post('/login', login)
+router.post('/', login)
 
 export default router
--- src/server.ts
+++ src/server.ts
@@ -7,12 +7,18 @@ import {
 	userRouter,
 } from './routes';
 import { errorHandler } from './middlewares/errorHandler';
+import cors from "cors"
 
 const app = express();
 const port = process.env.PORT || 3333;
 
 // Middleware
 app.use(express.json());
+app.use(cors({
+	origin: "http://localhost:5173",
+	allowedHeaders: ["Content-Type", "Authorization"],
+	methods: ["GET", "POST", "PUT", "DELETE"]
+}))
 
 // Routes
 app.use('/users', userRouter);
